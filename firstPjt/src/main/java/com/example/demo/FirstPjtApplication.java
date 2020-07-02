caf5162e8ef450b373c59c32071da7c34fe841a2
mzc01-jkcha <mzc01-jkcha@172.23.1.81>
2020-07-02 17:08:17 +0900

    init

---
+++ firstPjt/src/main/java/com/example/demo/FirstPjtApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class FirstPjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstPjtApplication.class, args);
+	}
+	@RequestMapping(path="")
+	public String test() {
+		return "2nd test";
+	}
+
+}
