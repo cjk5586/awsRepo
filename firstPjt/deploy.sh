16fe3205301452a4eb8277c60b5ef7ea46ea233c
cjk5586 <58755305+cjk5586@users.noreply.github.com>
2020-07-09 14:06:31 +0900

    Update deploy.sh

--- firstPjt/deploy.sh
+++ firstPjt/deploy.sh
@@ -4,7 +4,7 @@ JAR_NAME=$(basename $BUILD_PATH)
 echo "> build 파일명: $JAR_NAME"
 
 echo "> build 파일 복사"
-DEPLOY_PATH=/home/ec2-user/
+DEPLOY_PATH=/home/ubuntu/
 cp $BUILD_PATH $DEPLOY_PATH
 
 echo "> springboot-deploy.jar 교체"
